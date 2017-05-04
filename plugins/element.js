acf98769e063d9ceeee33c8368279dbc1c87638d
HelloWorld <HelloWorld>
2017-05-04 03:23:28 -0300

    Configuração do element no projeto

---
+++ plugins/element.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import 'element-ui/lib/theme-default/index.css'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/pt-br'
+Vue.use(ElementUI, { locale })
