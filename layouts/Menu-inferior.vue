a8d31fed900c5ac8163c247f28d63cf1a8552661
Gustavo Soriano <sorianox2010@hotmail.com>
2017-05-05 17:00:28 -0300

    Transição do menu e correção no caminho das imagens

--- layouts/Menu-inferior.vue
+++ layouts/Menu-inferior.vue
@@ -2,11 +2,11 @@
     <el-row>
 
         <el-col id="parentLogo" :xs="{span:7, offset:1}" :sm="{span:4, offset:1}">
-            <img src="../assets/logo.svg" />
+            <img src="../assets/logos/logo.svg" />
         </el-col>
         
         <el-col id="menu-inf"  :sm="{span:11, offset:1}">
-           <div id="active"></div>
+           <div id="active" :style="{opacity:opacidade, width:largura,left:distancia}" ></div>
            <span @mouseenter="animateActive" @mouseout="hideActive">Institucional</span>
            <span @mouseenter="animateActive" @mouseout="hideActive">Cursos</span>
            <span @mouseenter="animateActive" @mouseout="hideActive">Área acadêmica</span>
@@ -15,8 +15,8 @@
         </el-col>
 
         <el-col id="logomarcas" :sm="5">
-            <img src="../assets/cps.svg" />
-            <img src="../assets/gov.svg" />
+            <img src="../assets/logos/cps.svg" />
+            <img src="../assets/logos/gov.svg" />
         </el-col>
 
         <el-col id="menu-mobile" :xs="{span:5, offset:10}" :sm="{span:5, offset:14}">
@@ -30,7 +30,10 @@
   export default {
     data(){
         return{
-            menuAtivo:false
+            menuAtivo:false,
+            opacidade:'0',
+            largura:'0px',
+            distancia:'0'
         }
     },
 
@@ -54,13 +57,13 @@
 
 
 <style>
-#active{height:100%;width:110px;background-color:#b01116;z-index:2;position:absolute;transition:1s;opacity:0;}
+#active{height:3px;bottom:0;width:110px;background-color:#b01116;z-index:2;position:absolute;transition:150ms ease-in-out;opacity:0;}
 #parentLogo{height:15vh;display:flex;align-items:center;}
 #parentLogo img{width:95%;height:75%;max-height:85%;}
 
 #menu-inf{display:none;height:10vh;margin-top:2vh;font-size:.9em;color:#4A555C;}
 #menu-inf span{z-index:3;padding-left:2%;padding-right:2%;cursor:pointer;padding-top:2vh;}
-#menu-inf span:hover{color:#fff;transition:3s;}
+#menu-inf span:hover{color:#b01116;transition:1s;}
 
 #menu-mobile{height:15vh;display:flex;align-items:center;justify-content:space-around;}
 #menu-mobile i{border:2px solid #b01116;padding:3%;font-size:3em;color:#b01116;border-radius:5px;box-sizing: border-box;cursor:pointer;}
