2171d30f9947ffcf3e8d5dceb1eff94a6e90422c
01lucas-costa <01lucascosta@gmail.com>
2017-05-31 00:58:27 -0300

    criando-componente-corpo-docente

---
+++ layouts/cursos/CorpoDocente.vue
@@ -0,0 +1,70 @@
+<template>
+    <el-row class="corpo-doc">
+
+        <el-col class="header" :span="24">
+            <h1>Corpo Docente</h1>
+            <p>Conheça os professores desse curso.</p>
+        </el-col>
+
+        <el-col class="box-prof" :span="20" :offset="2">
+            <img class="img-prof" src="http://arquivos.biblioceeteps.com.br/p/erick.petrucelli.jpg?v=20170302125042&h=90">
+            <div class="content-prof">
+                <h4>Erick Eduardo Petrucelli</h4>
+                <p><b>Coordenador do Curso</b> | Mestre em Engenharia de Produçao</p>
+                <p><a><i></i>erick.petrucelli@fatectq.edu.br</a>  <a><i></i>Acessar currículo Lattes</a></p>
+                <p>Disciplinas: <span>Programação para Sítios de Internet</span> <span>Tópicos Especiais I</span> <span>Tópicos Especiais II</span></p>
+            </div>
+        </el-col>
+
+    </el-row>
+</template>
+
+<script>
+</script>
+
+<style>
+    .corpo-doc {       
+        background-color:#f5f5f5;         
+        font-size: 0.9em;           
+    }
+
+    .header {
+        text-align: center;
+        margin-top: 3%;
+        margin-bottom: 5%;
+    }
+
+    .header h1 {
+        color: #00a0e6;
+    }
+
+    .box-prof {
+        
+    }
+
+    .img-prof {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        display: block;
+        margin: auto;
+        margin-bottom: -20px;
+        border: 1px solid #00a0e6;
+    }
+
+    .content-prof {
+        background-color: #fff;
+        text-align: center;
+        font-size: 0.8em;
+    }
+
+    .content-prof h4 {
+        padding-top: 25px;
+        margin-bottom: 5px;
+        
+    }
+
+    .content-prof b, .content-prof a {
+        color: "#00a0e6";
+    }
+</style>
--- pages/cursos/index.vue
+++ pages/cursos/index.vue
@@ -23,41 +23,7 @@
             </el-col>
         </el-row>
 
-        <el-row class="corpo-doc">
-            <el-col class="titulos" :lg="{span:20, offset:2}">
-                <h1>Corpo Docente</h1>
-                <p>Conheça os professores desse curso.</p>
-            </el-col>
-
-            <el-col class="profs" :span="20" :offset="2">
-                <div class="content-profs">
-                    <img class="img-prof" src="http://arquivos.biblioceeteps.com.br/p/erick.petrucelli.jpg?v=20170302125042&h=90">
-                    <el-col class="text-profs">
-                        <h3>Erick Eduardo Petrucelli</h3>
-                        <p><b style="color:#00a0e6">Coordenador do Curso</b> | Mestre em Engenharia de Produçao</p>
-                        <p><a style="color:#00a0e6"><i class="el-icon-message"></i>erick.petrucelli@fatectq.edu.br</a>  <a style="color:#00a0e6"><i class="el-icon-information"></i>Acessar currículo Lattes</a></p>
-                        <p>Disciplinas: <span>Programação para Sítios de Internet</span> <span>Tópicos Especiais I</span> <span>Tópicos Especiais II</span></p>
-                    </el-col>
-                </div>
-            </el-col>
-
-            <el-col class="profs" :span="20" :offset="2">
-                <div class="content-profs">
-                    <img class="img-prof" src="http://arquivos.biblioceeteps.com.br/p/fabio.takeda.jpg?v=20121213182944&h=90">
-                    <el-col class="text-profs">
-                        <h3>Fábio Bento Takeda</h3>
-                        <p>Mestre em Ciência da Computação |  Professor de Ensino Superior II - A </p>
-                        <p><a style="color:#00a0e6"><i class="el-icon-message"></i>fabio.takeda@fatectq.edu.br</a>  <a style="color:#00a0e6"><i class="el-icon-information"></i>Acessar currículo Lattes</a></p>
-                        <p>Disciplinas: <span>Estruturas de Dados</span> <span>Trabalho de Graduação em Sistemas para Internet</span></p>
-                    </el-col>
-                </div>
-            </el-col>
-        </el-row>
-         <el-row>
-            <el-col style="height:3vh" class="barra-content" :span="24">
-                
-            </el-col>
-        </el-row>
+        <corpo-docente></corpo-docente>
         <app-footer></app-footer>
     </el-row> 
 </template>
@@ -67,12 +33,13 @@
     import CursoBanner from '../../layouts/cursos/Banner.vue'
     import CursoContent from '../../layouts/cursos/Content.vue'
     import AppApoio from '../../layouts/Apoio.vue';
+    import CorpoDocente from '../../layouts/cursos/CorpoDocente.vue'
     import AppFooter from '../../layouts/Footer.vue';
     import menuMobile from   '../../layouts/Menu-mobile.vue';
 
     export default {
         components: {
-            AppTopo, CursoBanner, CursoContent, AppApoio, AppFooter, menuMobile
+            AppTopo, CursoBanner, CursoContent, AppApoio, CorpoDocente, AppFooter, menuMobile
         }
     }
 
@@ -87,12 +54,9 @@
         margin-top: -5px;
     }
 
-    .corpo-doc {
-        padding-top:30px;
-        padding-bottom:30px;
-        background-color:#f5f5f5;        
-        padding-bottom: 20vh;
-    
+    #img-carrocel {
+        width: 100%;
+        margin-top: 5vh;
     }
 
     .titulos {
@@ -102,49 +66,6 @@
     .titulos h1 {
         color: #00a0e6;
         font-size: 2.5em;
-    }
-
-    #img-carrocel {
-        width: 100%;
-        margin-top: 5vh;
-    }
-
-    .profs {
-        margin-top: 3vh;
-    }
-
-    .img-prof {
-        margin-left: -45px;
-        margin-top: -45px;
-        width:90px;
-        height:90px;
-        border-radius:50%;
-        border: 3px solid #00a0e6;
-    }
-
-    .content-profs {
-        background: #fff;
-        margin-top: 45px;
-        margin-left: 45px;
-        display: flex;
-    }
-
-    .text-profs {
-        margin-left: 1vh;
-        margin-top: 1vh;
-        margin-bottom: 3vh;
-    }
-
-    .text-profs a {
-        padding: 2px;
-        margin: 2px;
-    }
-
-    .text-profs p span {
-        background: gray;
-        margin: 1vh;
-        border-radius: 5%;
-        color: #fff;
     }
 
 </style>
