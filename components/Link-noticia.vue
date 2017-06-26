1be634fa6f8c613a261177ca0deb9258148e31ad
Gustavo Soriano <sorianox2010@hotmail.com>
2017-06-25 22:13:13 -0300

    Componentizando link de notícias

---
+++ components/Link-noticia.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="item-noticia">
+        <img :src="img" >
+        <p>{{texto}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:["img","texto"]
+    }
+</script>
+
+<style>
+    .item-noticia{
+        width:100%;
+        margin-bottom:30px;
+    }
+
+    .item-noticia img{
+        width:100%;
+        max-width:500px;
+        height:auto;
+        border-radius:4px;
+    }
+
+    .item-noticia p{
+        font-size:1em !important;
+    }
+
+    @media screen and (min-width: 768px)
+    {
+        .item-noticia{
+            width:48%;
+            margin-bottom:10px;
+        }
+    }
+
+    @media screen and (min-width: 992px)
+    {
+        .item-noticia{
+            width:25%;
+            padding:1%;
+        }
+    }
+</style>
--- layouts/Noticias.vue
+++ layouts/Noticias.vue
@@ -5,98 +5,67 @@
             <p>Fique por dentro do que acontece na Fatec</p>
 
             <div id="content-noticias">
-
-                <div class="item-noticia">
-                    <img src="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" >
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-                <div class="item-noticia">
-                    <img src="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" >
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-                <div class="item-noticia">
-                    <img src="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" >
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-                <div class="item-noticia">
-                    <img src="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" >
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-                <div class="item-noticia">
-                    <img src="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" >
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
+                <link-noticia texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit!" img="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" ></link-noticia>
+                <link-noticia texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit!" img="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" ></link-noticia>
+                <link-noticia texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit!" img="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" ></link-noticia>
+                <link-noticia texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit!" img="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" ></link-noticia>
+                <link-noticia texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit!" img="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" ></link-noticia>
+                <link-noticia texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit!" img="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" ></link-noticia>
+                <link-noticia texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit!" img="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" ></link-noticia>
+                <link-noticia texto="Lorem ipsum dolor sit amet, consectetur adipisicing elit!" img="http://www.ingaia.com.br/wp-content/uploads/2017/01/tecnologia-os-5-beneficios-da-computacao-em-nuvem-para-as-imobiliarias-1.jpeg" ></link-noticia>
             </div>
 
         </el-col>
     </el-row>
 </template>
 
+<script>
+    import linkNoticia from '../components/Link-noticia.vue'
+
+    export default {
+        components:{linkNoticia}
+    }
+</script>
 
 <style>
-#noticias{
-    text-align:center;
-    padding-top:30px;
-}
-
-#noticias h1{
-    color:#b01116;
-    font-size:2.5em;
-}
-
-#noticias p{
-    color:#4A555C;
-    font-size:1.4em;
-    margin-bottom:10px;
-}
-
-#content-noticias{
-    width:84%;
-    min-height:300px;
-    margin:auto;
-}
-
-.item-noticia{
-    width:100%;
-    margin-bottom:30px;
-}
-
-.item-noticia img{
-    width:100%;
-    max-width:500px;
-    height:auto;
-    border-radius:4px;
-}
-
-.item-noticia p{
-    font-size:1em !important;
-}
-
-
-/* MD */
-@media screen and (min-width: 768px)
-{
-    .item-noticia{
-        width:48%;
-        margin-bottom:0px;
+    #noticias{
+        text-align:center;
+        padding-top:30px;
     }
 
-    #content-noticias{
-        display:flex;
-        flex-wrap:wrap;
-        justify-content:space-between;
+    #noticias h1{
+        color:#b01116;
+        font-size:2.5em;
     }
 
-}
-
-/* LG */
-@media screen and (min-width: 992px)
-{
-    .item-noticia{
-        width:24%;
+    #noticias p{
+        color:#4A555C;
+        font-size:1.4em;
+        margin-bottom:10px;
     }
+
     #content-noticias{
         width:82%;
+        min-height:300px;
+        margin:auto;
     }
-}
+
+    @media screen and (min-width: 768px)
+    {
+        #content-noticias{
+            display:flex;
+            flex-wrap:wrap;
+            justify-content:space-between;
+        }
+    }
+
+    @media screen and (min-width: 992px)
+    {
+        #content-noticias{
+            width:82%;
+            justify-content:flex-start;
+        }
+    }
+
+
 </style>
